4371a94e906b103b6e9d4fcf2ec84eed9576c01b
Spencer Price <spencerprice@csu.fullerton.edu>
2022-12-14 19:46:22 -0800

    Working Repo w/ Tailwind, Pocketbase, Browsersync, Esbuild

---
+++ src/app.jsx
@@ -0,0 +1,24 @@
+const USERNAME = 'spencerprice@csu.fullerton.edu'
+const PASSWORD = 'Project_04'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
+console.log(authData)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Good luck, Guys. Im counting on you.</h1>
+    </div>
+  )
+}
+
