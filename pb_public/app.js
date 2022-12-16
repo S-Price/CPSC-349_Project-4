b31cf40b05f273f638b6233aa64201bb7f34f978
misterpurple98 <misterpurple98@gmail.com>
2022-12-16 09:54:01 -0800

    Integrated navigation

--- pb_public/app.js
+++ pb_public/app.js
@@ -8,5 +8,5 @@ root.render(
   /* @__PURE__ */ React.createElement(React.StrictMode, null, /* @__PURE__ */ React.createElement(App, null))
 );
 function App() {
-  return /* @__PURE__ */ React.createElement("div", { className: "App" }, /* @__PURE__ */ React.createElement("h1", null, "Good luck, Guys. Im counting on you."));
+  return /* @__PURE__ */ React.createElement("div", { className: "App" }, /* @__PURE__ */ React.createElement("h1", null, "Good luck, Guys. Im counting on you."), /* @__PURE__ */ React.createElement("h1", null, "Test Test"));
 }
